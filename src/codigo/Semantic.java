45e90bcb49e116f84c26c3b69758aeb58489f792
kvnsnchz <kvn9616@gmail.com>
2019-10-07 05:06:46 +0200

    Validando que sean de tipo logico

--- src/codigo/Semantic.java
+++ src/codigo/Semantic.java
@@ -40,49 +40,74 @@ public class Semantic {
                 ElementoTablaS elem = tablaS.buscar(nodo.getIdentificador());
                 
                 if(!(elem != null && elem.getNumSent() <= numSent)){
-                    errores.add("La variable "+nodo.getIdentificador()+" no ha sido declarada.");
+                    errores.add("La variable "+nodo.getIdentificador()+" no ha sido declarada.\n");
                 }
                 else{
                     if(elem != null && elem.getIsVector() &&  nodo.getIndiceVector() == null){
-                        errores.add("Uso incorrecto del vector "+((NodoAsignacion)raiz).getIdentificador()+" como variable simple.");
+                        errores.add("Uso incorrecto del vector "+((NodoAsignacion)raiz).getIdentificador()+" como variable simple.\n");
                     }
                     else{
                         if(elem != null && !elem.getIsVector() &&  nodo.getIndiceVector() != null){
-                            errores.add("Uso incorrecto de la variable simple "+((NodoAsignacion)raiz).getIdentificador()+" como vector.");
+                            errores.add("Uso incorrecto de la variable simple "+((NodoAsignacion)raiz).getIdentificador()+" como vector.\n");
+                        }
+                    }
+                    if(nodo.getAsignacion() instanceof NodoOperacion){
+                        if(((NodoOperacion)nodo.getAsignacion()).getTipo() !=  elem.getTipo()){
+                            errores.add("La variable " + nodo.getIdentificador() + " es de tipo " + elem.getTipo() + " no se le puede asignar un valor de tipo " + ((NodoOperacion)nodo.getAsignacion()).getTipo() + ".\n");
                         }
                     }
                 }
                 
-                //Falta verificacion del tipo cuando se asigna
+                //Falta verificacion del tipo cuando se asigna directamente un elemento
             }
             else{
                 if(raiz instanceof NodoPara){
                     NodoPara nodo = (NodoPara)raiz;
                     
                     if(!(nodo.getInicializador() instanceof NodoAsignacion)){
-                        errores.add("La inicializacion del ciclo para debe ser una asignacion");
+                        errores.add("La inicializacion del ciclo para debe ser una asignacion.\n");
                     }
                     
                     if(!(nodo.getPaso()instanceof NodoAsignacion)){
-                        errores.add("El paso del ciclo para debe ser una asignacion");
+                        errores.add("El paso del ciclo para debe ser una asignacion.\n");
                     }
                     
-                    //Falta la verificacion de la verificacion
+                    if(nodo.getVerificacion() instanceof NodoOperacion){
+                        if(((NodoOperacion)nodo.getVerificacion()).getTipo() != TiposIds.logico){
+                            errores.add("La verificacion del ciclo para debe ser una expresion logica.\n");
+                        }
+                    }
+                    //Falta la verificacion de la verificacion cuando es un elemento simple
                 }
                 else{
                     if(raiz instanceof NodoFuncion){
                         NodoFuncion nodo = (NodoFuncion)raiz;
                         
                         if(!tiposFunc.contains(nodo.getIdentificador())){
-                            errores.add("La funcion " + nodo.getIdentificador() + " no existe.");
+                            errores.add("La funcion " + nodo.getIdentificador() + " no existe.\n");
                         }
                         
                     }
                     else{
-                        if(raiz instanceof NodoSi){
-                            NodoSi nodo = (NodoSi)raiz;
-                            if(((NodoOperacion)nodo.getCondicion()).getTipo() != TiposIds.logico){
-                                errores.add("La condicion del si debe ser una expresion logica.");
+                        if(raiz instanceof NodoRepita){
+                            NodoRepita nodo = (NodoRepita)raiz;
+                            if(nodo.getCondicion() instanceof NodoOperacion){
+                                if(((NodoOperacion)nodo.getCondicion()).getTipo() != TiposIds.logico){
+                                    errores.add("La condicion del ciclo repita debe ser una expresion logica.\n");
+                                }
+                            }
+                            //Falta la verificacion de la condicion cuando es un elemento simple
+                        }
+                        else{
+                             if(raiz instanceof NodoSi){
+                                NodoSi nodo = (NodoSi)raiz;
+                                
+                                if(nodo.getCondicion() instanceof NodoOperacion){
+                                    if(((NodoOperacion)nodo.getCondicion()).getTipo() != TiposIds.logico){
+                                        errores.add("La condicion del si debe ser una expresion logica.\n");
+                                    }
+                                }
+                            //Falta la verificacion de la condicion cuando es un elemento simple
                             }
                         }
                     }
