ffed00cc925334d5e537d3f2871281f84caf2f75
Javier Martinez <javier.martinez@alegra.com>
2019-10-08 17:17:46 +0200

    Descartando Carpeta build y add Clase GenerarCodigo

---
+++ src/codigo/GenerarCodigo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import arbol.*;
+import java.util.Vector;
+
+/**
+ *
+ * @author andvri
+ */
+public class GenerarCodigo {
+    static Vector<String> salida;
+    
+    
+    public GenerarCodigo () {
+        this.salida = new Vector<String>();
+    }
+    
+    public String generarSalida(NodoBase raiz, TablaSimbolos ts) {
+        return null;
+    }
+    
+    public Vector<String> getSalida() {
+        return this.salida;
+    }
+}
